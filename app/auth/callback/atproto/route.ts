b1659d337341378ccdd405d7d5c4cbca05ab0411
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-12-17 21:43:47 +0900

    redirect after login

--- app/auth/callback/atproto/route.ts
+++ app/auth/callback/atproto/route.ts
@@ -1,15 +1,15 @@
-import { Agent } from "@atproto/api";
 import { NextRequest } from "next/server";
 import client from "@/lib/client";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export const GET = async (req: NextRequest) => {
   const params = req.nextUrl.searchParams;
+  const cookie = await cookies();
 
-  const { session } = await client.callback(params);
-  const agent = new Agent(session);
-
-  // Make Authenticated API calls
-  const profile = await agent.getProfile({ actor: agent.did! });
-
-  return Response.json(profile.data);
+  const { session: { sub } } = await client.callback(params);
+  cookie.set("did", sub);
+  const redirectTo = cookie.get("redirectTo")?.value ?? "/";
+  cookie.delete("redirectTo");
+  return redirect(redirectTo);
 };
--- app/auth/route.ts
+++ app/auth/route.ts
@@ -1,7 +1,20 @@
 import client from "@/lib/client";
+import { getParam } from "@/lib/utils";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
+const setRedirectToFromSearchParam = async (req: NextRequest) => {
+  await getRedirectToFrom("search")(req).then(setRedirectTo);
+  return req;
+};
+const setRedirectToFromBody = async (req: NextRequest) => {
+  await getRedirectToFrom("body")(req).then(setRedirectTo);
+  return req;
+};
+const getRedirectToFrom = getParam("redirectTo");
+const setRedirectTo = (redirectTo?: string | undefined | null): unknown =>
+  redirectTo && cookies().then((c) => c.set("redirectTo", redirectTo));
 const getHandleFromSearchParam = (req: NextRequest) =>
   req.nextUrl.searchParams.get("handle");
 const getHandleFromBody = (req: NextRequest) =>
@@ -18,7 +31,11 @@ const redirectToAuthorize = async (handle?: string | null) =>
     : redirect("/auth/login");
 
 export const GET = async (req: NextRequest) =>
-  redirectToAuthorize(getHandleFromSearchParam(req));
+  setRedirectToFromSearchParam(req)
+    .then(getHandleFromSearchParam)
+    .then(redirectToAuthorize);
 
 export const POST = async (req: NextRequest) =>
-  getHandleFromBody(req).then(redirectToAuthorize);
+  setRedirectToFromBody(req)
+    .then(getHandleFromBody)
+    .then(redirectToAuthorize);
