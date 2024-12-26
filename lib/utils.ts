0503d22eef2fa8986cc10490fb5589313f23f3f0
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-12-26 22:26:15 +0900

    init shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,6 @@
-import { NextRequest } from "next/server";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export type GetParamFrom = "search" | "body";
-export const getParam: //
-  (key: string) => //
-  (where: GetParamFrom) => //
-  (req: NextRequest) => Promise<string | null> =
-    (key) => (where) => async (req) =>
-      where === "body"
-        ? ((await req.json())[key] ?? null)
-        : req.nextUrl.searchParams.get(key);
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
