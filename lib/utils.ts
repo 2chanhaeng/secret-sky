6347bfef5dbde3185e7e14db8d65c9aac1fd0da0
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-12-27 02:19:56 +0900

    recover removed code

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { NextRequest } from "next/server";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export type GetParamFrom = "search" | "body";
+export const getParam: //
+(key: string) => //
+(where: GetParamFrom) => //
+(req: NextRequest) => Promise<string | null> =
+  (key) => (where) => async (req) =>
+    where === "body"
+      ? (await req.json())[key] ?? null
+      : req.nextUrl.searchParams.get(key);
