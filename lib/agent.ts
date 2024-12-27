20580e8aaf38ee16794c7aea568d842e0bad9c03
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-12-27 14:28:12 +0900

    add redirectTo prop

--- lib/agent.ts
+++ lib/agent.ts
@@ -4,13 +4,15 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const getAgent: //
-  (client: NodeOAuthClient) => Promise<Agent> = //
-  async (client) => {
-    const did = (await cookies()).get("did")?.value;
-    if (!did) redirect("/auth/login");
-    const session = await client
-      .restore(did)
-      .catch(() => redirect("/auth/login"));
-    const agent = new Agent(session);
-    return agent;
-  };
+(client: NodeOAuthClient, redirectTo?: string) => Promise<Agent> = async ( //
+  client,
+  redirectTo = "/"
+) => {
+  const did = (await cookies()).get("did")?.value;
+  if (!did) redirect("/auth/login");
+  const session = await client
+    .restore(did)
+    .catch(() => redirect(`/auth/login?redirectTo=${redirectTo}`));
+  const agent = new Agent(session);
+  return agent;
+};
