272a7ce45b835ffeead31320ebcf95839604f0c5
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-12-27 14:30:55 +0900

    define getUri util

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,12 +1,5 @@
-import { post } from "@/actions/post";
-import { decrypt } from "@/lib/aes";
-import { getAgent } from "@/lib/agent";
-import client from "@/lib/client";
-import prisma from "@/prisma";
-import { isLink } from "@atproto/api/dist/client/types/app/bsky/richtext/facet";
-import Form from "next/form";
-import { notFound } from "next/navigation";
-import { isReadable } from "./utils";
+import { getUri } from "./utils";
+import Thread from "@/components/Thread";
 
 export default async function PostPage({
   params,
@@ -14,46 +7,11 @@ export default async function PostPage({
   params: Promise<{ postId: string }>;
 }) {
   const { postId } = await params;
-  const { uri, key, iv, list } = await prisma.post
-    .findUniqueOrThrow({
-      where: { id: postId },
-      include: { list: { select: { uri: true } } },
-    })
-    .catch(notFound);
-  if (!uri) notFound();
-  const agent = await getAgent(client);
-  const [repo, , rkey] = uri.split("/").slice(-3);
-  const {
-    data: { did, handle, avatar, displayName },
-  } = await agent.getProfile({ actor: repo });
-  if (!(agent.did === did || (await isReadable(list.uri)))) notFound();
-  const {
-    value: { facets },
-  } = await agent.getPost({ repo, rkey }).catch(notFound);
-  if (!facets) notFound();
-  const links = facets.flatMap(({ features }) => features).filter(isLink);
-  const link = links.find((facet) =>
-    facet.uri.match(/\/posts\/\w+\?value=(.+)$/)
-  );
-  if (!link) notFound();
-  const encrypted = new URLSearchParams(link.uri.split("?")[1]).get("value");
-  if (!encrypted) notFound();
-  const content = await decrypt(encrypted, key, iv);
+  const uri = await getUri(postId);
 
   return (
     <main>
-      <h1>{displayName ?? handle}</h1>
-      <img src={avatar} alt={displayName} />
-      <p>{handle}</p>
-      <a href={`https://bsky.app/profile/${did}`}>프로필</a>
-      <p>{content}</p>
-      <Form action={post}>
-        <textarea name="content" />
-        <textarea name="open" />
-        <input type="hidden" name="list" value={list.uri} />
-        <input type="hidden" name="parent" value={uri} />
-        <button type="submit">Post</button>
-      </Form>
+      <Thread main={uri} />
     </main>
   );
 }
--- app/posts/[postId]/utils.ts
+++ app/posts/[postId]/utils.ts
@@ -1,58 +1,11 @@
-import { getAgent } from "@/lib/agent";
-import client from "@/lib/client";
-import { Agent } from "@atproto/api";
+import { notFound } from "next/navigation";
+import prisma from "@/prisma";
 
-export const isReadable = async (list: string): Promise<boolean> => {
-  const agent = await getAgent(client);
-  const { did } = agent;
-  if (!did) return false;
-  return hasTarget(did)(getList(agent)(list));
-};
-
-interface UserScrapper {
-  (list: string): (cursor?: string) => Promise<string[]>;
-}
-
-const getList: //
-  (agent: Agent) => UserScrapper = //
-  (agent) => (list) =>
-    list.startsWith("at://")
-      ? getUserList(agent)(list)
-      : list.endsWith("/followers")
-      ? getFollowers(agent)(list.split("/")[0])
-      : getFollows(agent)(list.split("/")[0]);
-
-const getUserList: //
-  (agent: Agent) => UserScrapper = //
-  (agent) => (list) => (cursor) =>
-    agent.app.bsky.graph.getList({ list, cursor })
-      .then(({ data: { items } }) => items)
-      .then((items) => items.map(({ subject: { did } }) => did));
-
-const getFollowers: //
-  (agent: Agent) => UserScrapper = //
-  (agent) => (actor) => (cursor) =>
-    agent.app.bsky.graph.getFollowers({ actor, cursor })
-      .then(({ data: { followers } }) => followers)
-      .then((followers) => followers.map(({ did }) => did));
-
-const getFollows: //
-  (agent: Agent) => UserScrapper = //
-  (agent) => (actor) => (cursor) =>
-    agent.app.bsky.graph.getFollows({ actor, cursor })
-      .then(({ data: { follows } }) => follows)
-      .then((follows) => follows.map(({ did }) => did));
-
-const hasTarget //
- = (target: string) =>
-async (f: (cursor?: string) => Promise<string[]>): Promise<boolean> => {
-  let scraped: string[];
-  let cursor: string | undefined;
-  while (scraped = await f(cursor)) {
-    const scrapedSet = new Set(scraped);
-    if (scrapedSet.has(target)) return true;
-    if (scraped.length < 2 || scrapedSet.has(cursor ?? "")) return false;
-    cursor = scraped.pop();
-  }
-  return false;
-};
+export const getUri = (postId: string) =>
+  prisma.post
+    .findUniqueOrThrow({
+      where: { id: postId },
+      select: { uri: true },
+    })
+    .catch(notFound)
+    .then((post) => post.uri ?? notFound());
