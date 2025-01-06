4fa9d25212d70292b884d2c14f50b548c0de9292
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-01-06 21:36:08 +0900

    decorate root page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,34 +2,56 @@ import client from "@/lib/client";
 import Form from "next/form";
 import { getAgent } from "@/lib/agent";
 import { post } from "@/actions/post";
+import AuthorAvatar from "@/components/AuthorAvatar";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const agent = await getAgent(client);
+  const agent = await getAgent(client, "/");
   const { did } = agent;
   const { data } = await agent.getProfile({ actor: agent.did! });
-  const { handle, avatar, displayName } = data;
+  // const { handle, avatar, displayName } = data;
   const lists = [
     { name: "팔로잉 / 내가 팔로우하는 사람들", uri: `${did}/followings` },
     { name: "팔로워 / 나를 팔로우하는 사람들", uri: `${did}/followers` },
   ];
   return (
     <main>
-      <h1>{displayName ?? handle}</h1>
-      <img src={avatar} alt={displayName} />
-      <p>{handle}</p>
-      <a href={`https://bsky.app/profile/${did}`}>프로필</a>
-      <Form action={post}>
-        <textarea name="content" />
-        <textarea name="open" />
-        <select defaultValue={lists[0].uri} name="list">
-          {lists.map(({ uri, name }) => (
-            <option key={uri} value={uri}>
-              {name}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Post</button>
-      </Form>
+      <section className="flex">
+        <AuthorAvatar {...data} />
+        <Form action={post} className="flex flex-col ml-2 gap-2">
+          <Textarea name="content" placeholder="무슨 일이 일어나고 있나요?" />
+          <Textarea
+            name="open"
+            placeholder="어떤 글인지 공개할 내용을 적어주세요."
+          />
+          <Select name="list" defaultValue={lists[0].uri}>
+            <SelectTrigger>
+              <SelectValue placeholder="리스트" />
+            </SelectTrigger>
+            <SelectContent>
+              {lists.map(({ uri, name }) => (
+                <SelectItem key={uri} value={uri}>
+                  {name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Button
+            type="submit"
+            className="bg-foreground text-background font-bold"
+          >
+            Post
+          </Button>
+        </Form>
+      </section>
     </main>
   );
 }
