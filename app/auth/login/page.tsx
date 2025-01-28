15efeb59c08e207f9adbc86d3ad2be9264fd7f57
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-01-28 15:56:21 +0900

    decorate login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import Form from "next/form";
 
 export default async function LoginPage({
@@ -9,11 +12,27 @@ export default async function LoginPage({
 
   return (
     <main>
-      <Form action="/auth">
-        <input type="text" name="handle" autoComplete="handle" />
-        <input type="hidden" name="redirectTo" value={redirect ?? "/"} />
-        <button type="submit">Login</button>
-      </Form>
+      <Card>
+        <CardHeader className="text-center text-2xl">Login</CardHeader>
+        <CardContent>
+          <Form action="/auth" className="flex flex-col gap-2">
+            <Input
+              type="text"
+              name="handle"
+              autoComplete="handle"
+              placeholder="Handle"
+              className="text-2xl"
+            />
+            <input type="hidden" name="redirectTo" value={redirect ?? "/"} />
+            <Button
+              type="submit"
+              className="bg-foreground text-background font-bold"
+            >
+              Login
+            </Button>
+          </Form>
+        </CardContent>
+      </Card>
     </main>
   );
 }
