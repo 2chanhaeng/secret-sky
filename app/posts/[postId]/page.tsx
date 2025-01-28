8bdbc0164e1549ddc98ce2299dd393138089ef64
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-01-29 05:27:39 +0900

    redirect if not logged in

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
 import { getUri } from "./utils";
 import Thread from "@/components/Thread";
+import { getAgent } from "@/lib/agent";
+import client from "@/lib/client";
 
 export default async function PostPage({
   params,
@@ -7,6 +9,7 @@ export default async function PostPage({
   params: Promise<{ postId: string }>;
 }) {
   const { postId } = await params;
+  await getAgent(client, `/posts/${postId}`);
   const uri = await getUri(postId);
 
   return (
