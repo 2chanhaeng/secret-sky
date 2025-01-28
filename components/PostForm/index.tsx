c806566465ae988a23961de038008db4251482b0
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-01-28 16:20:26 +0900

    decorate PostForm

--- components/PostForm/index.tsx
+++ components/PostForm/index.tsx
@@ -1,23 +1,38 @@
 import Form from "next/form";
 import { post } from "@/actions/post";
-// import ListSelect from "./ListSelect";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import AuthorAvatar, { AuthorAvatarProps } from "@/components/AuthorAvatar";
 
 export default function PostForm({
+  author,
   parent,
+  className,
 }: {
+  author: AuthorAvatarProps;
   parent?: { uri: string; list: string };
+  className?: string;
 }) {
   return (
-    <Form action={post}>
-      <textarea name="content" />
-      <textarea name="open" />
-      <input
-        type="hidden"
-        name="list"
-        value={parent ? parent.list : "followings"}
-      />
-      {parent && <input type="hidden" name="parent" value={parent.uri} />}
-      <button type="submit">Post</button>
-    </Form>
+    <section className={`flex gap-2 ${className}`}>
+      <AuthorAvatar {...author} className="size-12" />
+      <Form action={post} className="flex flex-col ml-2 gap-2 w-full">
+        <Textarea
+          name="open"
+          placeholder="공개적으로 적을 내용을 적어주세요."
+        />
+        <Textarea
+          name="content"
+          placeholder="비공개적으로 작성할 내용을 적어주세요."
+        />
+        {parent && <input type="hidden" name="parent" value={parent.uri} />}
+        <Button
+          type="submit"
+          className="bg-foreground text-background font-bold"
+        >
+          작성하기
+        </Button>
+      </Form>
+    </section>
   );
 }
