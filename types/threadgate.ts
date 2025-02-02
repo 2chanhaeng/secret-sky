45c2e581bc35df1cd073cd6ce0606d3561562f6f
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-02 21:31:41 +0900

    define threadgate types

---
+++ types/threadgate.ts
@@ -0,0 +1,15 @@
+export const MENTION_RULE = "app.bsky.feed.threadgate#mentionRule";
+export const FOLLOWING_RULE = "app.bsky.feed.threadgate#followingRule";
+export const LIST_RULE = "app.bsky.feed.threadgate#listRule";
+
+export interface MentionRule {
+  $type: typeof MENTION_RULE;
+}
+export interface FollowingRule {
+  $type: typeof FOLLOWING_RULE;
+}
+export interface ListRule {
+  $type: typeof LIST_RULE;
+  list: string;
+}
+export type ThreadgateType = MentionRule | FollowingRule | ListRule;
