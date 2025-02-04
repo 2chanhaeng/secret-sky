582681fb7f3a6f66069c97948793ee2fe7c3da56
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-04 20:00:23 +0900

    define PostRecord type

--- types/bsky.ts
+++ types/bsky.ts
@@ -1,5 +1,8 @@
 import { AppBskyRichtextFacet } from "@atproto/api";
 import { ProfileViewDetailed } from "@atproto/api/dist/client/types/app/bsky/actor/defs";
+export type {
+  Record as PostRecord,
+} from "@atproto/api/dist/client/types/app/bsky/feed/post";
 
 export type Facet = AppBskyRichtextFacet.Main;
 export type Profile = ProfileViewDetailed;
