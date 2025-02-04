14b24473ce3c2e43f53da59a744dde64f164594b
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-04 20:20:51 +0900

    fix post action

--- actions/post.ts
+++ actions/post.ts
@@ -5,84 +5,145 @@ import { getAgent } from "@/lib/agent";
 import client from "@/lib/client";
 import { URL_BASE } from "@/lib/url";
 import prisma from "@/prisma";
-import { Facet } from "@/types/bsky";
+import {
+  FOLLOWING_RULE,
+  FollowingRule,
+  LIST_RULE,
+  ListRule,
+  MENTION_RULE,
+  MentionRule,
+  ThreadgateType,
+} from "@/types/threadgate";
+import { Facet, PostRecord } from "@/types/bsky";
 import { Agent, RichText } from "@atproto/api";
 import { ReplyRef } from "@atproto/api/dist/client/types/app/bsky/feed/post";
 import { redirect } from "next/navigation";
+import { generateTID } from "@/lib/tid";
+
+const APPLY_WRITE_TYPE = "com.atproto.repo.applyWrites#create";
+const POST_TYPE = "app.bsky.feed.post";
+const THREADGATE_TYPE = "app.bsky.feed.threadgate";
 
 export const post = async (form: FormData) => {
   const agent = await getAgent(client);
+  const createdAt = new Date();
+  const repo = agent.assertDid;
+  const rkey = generateTID(createdAt);
 
+  const uri = `at://${repo}/${POST_TYPE}/${rkey}`;
   const {
     content,
-    list: rawList = "followings",
     parent,
     open,
   } = Object.fromEntries(form) as Record<string, string>; //
-  const list = getList(rawList, agent.did!);
-  const { id: listId, key } = await getOrCreateKey(list);
+  const key = await genKey();
   const { encrypted, iv } = await encrypt(content, key);
-  const { id } = await prisma.post.create({ data: { listId, key, iv } });
-  const uri = await createPost(agent)({ encrypted, id, open, parent });
-  if (rawList === "current") {
-    await prisma.list.update({ where: { id: listId }, data: { uri } });
-  }
-  await prisma.post.update({ where: { id }, data: { uri } });
+  const post = await createPostRecord(agent)({
+    encrypted,
+    repo,
+    open,
+    parent,
+    createdAt,
+    rkey,
+  });
+  const postWrites = {
+    $type: APPLY_WRITE_TYPE,
+    collection: POST_TYPE,
+    rkey,
+    value: post,
+  };
+  const allow = getThreadgate(form);
+  const threadgateWrites = {
+    $type: APPLY_WRITE_TYPE,
+    collection: THREADGATE_TYPE,
+    rkey,
+    value: {
+      $type: THREADGATE_TYPE,
+      post: uri,
+      createdAt: createdAt.toISOString(),
+      allow,
+    },
+  };
+  await agent.com.atproto.repo.applyWrites({
+    repo,
+    writes: [postWrites, threadgateWrites],
+    validate: true,
+  });
+
+  await prisma.post.create({ data: { key, iv, uri } });
   const href = uriToUrl(uri);
   redirect(href);
 };
 
-const getList = (rawList: string, did: string) => {
-  if (rawList.startsWith("at://")) return rawList;
-  if (rawList === "current") return `current/${crypto.randomUUID()}`;
-  return `${did}/${rawList}`;
-};
+const getThreadgate: (form: FormData) => ThreadgateType[] = (form) =>
+  (form.getAll("allow") as string[])
+    .map((rule) =>
+      rule.startsWith("at://")
+        ? ({
+          $type: LIST_RULE,
+          list: rule,
+        } as ListRule)
+        : ({
+          $type: rule as typeof MENTION_RULE | typeof FOLLOWING_RULE,
+        } as MentionRule | FollowingRule)
+    );
 
-const getOrCreateKey: //
-  (uri: string) => Promise<{ id: string; key: string }> = async (uri) =>
-    prisma.list.upsert({
-      where: { uri },
-      update: {},
-      create: { uri, key: await genKey() },
-      select: { id: true, key: true },
-    });
-
-const createPost: //
+const createPostRecord: //
   (agent: Agent) => //
-  (props: { encrypted: string; id: string; open?: string; parent?: string }) => //
-  Promise<string> = (agent) => async ({ encrypted, id, open = "", parent }) => {
+  (
+    props: {
+      encrypted: string;
+      repo: string;
+      rkey: string;
+      createdAt: Date;
+      open?: string;
+      parent?: string;
+    },
+  ) => //
+  Promise<PostRecord> = (agent) =>
+  async (
+    { encrypted, repo, open = "", parent, rkey, createdAt },
+  ) => {
     const text = createPostText(open);
     const rt = new RichText({ text });
     await rt.detectFacets(agent);
     if (rt.facets === undefined) rt.facets = [];
-    rt.facets.push(addOriginFacet(text, id, encrypted));
+    rt.facets.push(createLinkFacet({ text, repo, rkey }));
+    rt.facets.push(createEncryptFacet({ encrypted }));
     const reply = await getReply(agent)(parent);
 
-    const { uri } = await agent
-      .post({ text: rt.text, facets: rt.facets, reply })
-      .catch((e) => {
-        console.error("Post creation error", e);
-        throw e;
-      });
-    return uri;
+    return ({
+      $type: POST_TYPE,
+      text: rt.text,
+      facets: rt.facets,
+      createdAt: createdAt.toISOString(),
+      reply,
+    });
   };
 const TEXT_TO_LINK = "비밀글 보기";
 const createPostText = (open: string) =>
-  `#그늘셀프${open.length > 0 ? "\n\n" + open : ""}\n\n${TEXT_TO_LINK}`;
-const addOriginFacet: (post: string, id: string, encrypted: string) => Facet = (
-  //
-  post,
-  id,
-  encrypted,
+  `#그늘셀프${open ? "\n\n" + open : ""}\n\n${TEXT_TO_LINK}`;
+const createLinkFacet: (
+  props: { text: string; repo: string; rkey: string },
+) => Facet = (
+  { text, repo, rkey },
 ) => {
-  const uri = `${URL_BASE}/posts/${id}?value=${encrypted}`;
-  const byteStart = getByteLength(post.slice(0, -TEXT_TO_LINK.length));
-  const byteEnd = getByteLength(post);
+  const uri = `${URL_BASE}/profile/${repo}/post/${rkey}`;
+  const byteStart = getByteLength(text.slice(0, -TEXT_TO_LINK.length));
+  const byteEnd = getByteLength(text);
   return {
     index: { byteStart, byteEnd },
     features: [{ $type: "app.bsky.richtext.facet#link", uri }],
   };
 };
+
+const createEncryptFacet: (props: { encrypted: string }) => Facet = ({
+  encrypted,
+}) => ({
+  index: { byteStart: 0, byteEnd: 0 },
+  features: [{ $type: "app.bsky.richtext.facet#encrypt", encrypted }],
+});
+
 const getReply =
   (agent: Agent) => async (uri?: string): Promise<ReplyRef | undefined> => {
     if (!uri) return undefined;
