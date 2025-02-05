412e998413de80b85762b95a79381703bfe24c16
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-05 21:25:25 +0900

    set profile in cookie

--- app/auth/callback/atproto/route.ts
+++ app/auth/callback/atproto/route.ts
@@ -2,6 +2,8 @@ import { NextRequest } from "next/server";
 import client from "@/lib/client";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import { Profile } from "@/types/bsky";
+import { BaseProfile } from "@/types/profile";
 
 export const GET = async (req: NextRequest) => {
   const params = req.nextUrl.searchParams;
@@ -16,8 +18,34 @@ export const GET = async (req: NextRequest) => {
       return ({ session: { sub: "error" } });
     });
   if (sub === "error") return redirect("/auth/login");
-  cookie.set("did", sub);
+  const profile = await fetchProfile(sub);
+  const stringifiedProfile = JSON.stringify(pickProfile(profile));
+  cookie.set("did", profile.did, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  });
+  cookie.set("profile", stringifiedProfile, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  });
+
   const redirectTo = cookie.get("redirectTo")?.value ?? "/";
   cookie.delete("redirectTo");
   return redirect(redirectTo);
 };
+
+const fetchProfile = async (sub: string) =>
+  fetch(
+    "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=" + sub,
+  ).then((res) => res.json());
+
+const pickProfile = (
+  { did, handle, avatar = "", displayName }: Profile,
+): BaseProfile => ({
+  did,
+  handle,
+  avatar,
+  displayName: displayName ?? handle,
+});
