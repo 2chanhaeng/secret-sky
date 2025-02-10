884b7cd56df93807f5c1ce4ce7956edcf165f898
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-11 08:29:21 +0900

    define useFeedInfos hook

--- hooks/db.tsx
+++ hooks/db.tsx
@@ -6,10 +6,15 @@ import {
   useIndexedDB,
 } from "react-indexed-db-hook";
 import { useEffect, useState } from "react";
+import { useFeeds, usePref } from "./use-pref";
+import { GeneratorRecord, ListRecord, SavedFeed } from "@/types/bsky";
+import { getRecord } from "@/lib/api";
+import { isGeneratorRecord, isListRecord, isObj } from "@/lib/pred";
+import { parseAtUri } from "@/lib/uri";
 
 export const SecretSkyDBVersion1Schema: IndexedDBProps = {
   name: "SecretSkyDB",
-  version: 1,
+  version: 2,
   objectStoresMeta: [
     {
       store: "accounts",
@@ -25,8 +30,35 @@ export const SecretSkyDBVersion1Schema: IndexedDBProps = {
         },
       ],
     },
+    {
+      store: "feeds",
+      storeConfig: { keyPath: "uri", autoIncrement: false },
+      storeSchema: [
+        { name: "type", keypath: "type", options: { unique: false } },
+        { name: "uri", keypath: "uri", options: { unique: true } },
+        {
+          name: "displayName",
+          keypath: "displayName",
+          options: { unique: false },
+        },
+        {
+          name: "description",
+          keypath: "description",
+          options: { unique: false },
+        },
+        { name: "avatar", keypath: "avatar", options: { unique: false } },
+      ],
+    },
   ],
 } as const;
+
+interface DBFeed {
+  type: string;
+  uri: string;
+  displayName: string;
+  description: string;
+  avatar: string;
+}
 
 initDB(SecretSkyDBVersion1Schema);
 
@@ -55,3 +87,112 @@ export const useLoggedAccounts = (current: string) => {
   }, [db, current]);
   return accounts;
 };
+
+export const useDeleteAccount = (did: string) => {
+  const db = useIndexedDB("accounts");
+  return () => db.deleteRecord(did);
+};
+
+interface FeedInfo extends DBFeed {
+  pinned: boolean;
+}
+
+export const useFeedInfos = () => {
+  const db = useIndexedDB("feeds");
+  const prefFeeds = useFeeds();
+  const [feeds, setFeeds] = useState<FeedInfo[]>([]);
+  useEffect(() => {
+    const updateInfos = async () =>
+      db
+        .getAll<DBFeed>()
+        .then(async (dbFeeds) => {
+          const toFetch = prefFeeds.filter(
+            (f) =>
+              f.type !== "timeline" && !dbFeeds.find((df) => df.uri === f.value)
+          );
+          const fetches = toFetch.map(({ value }) => value).map(getRecord);
+          const values = (await Promise.all(fetches)).map(({ uri, value }) => ({
+            uri,
+            ...value,
+          }));
+          return values;
+        })
+        .then((values) =>
+          values
+            .filter((feed) => isListRecord(feed) || isGeneratorRecord(feed))
+            .forEach((feed) => db.add<DBFeed>(pickFeed(feed)))
+        );
+    const getInfos = async () =>
+      db.getAll<DBFeed>().then((dbFeeds) => {
+        const newFeeds = prefFeeds
+          .map(({ value: uri, pinned }) => {
+            if (uri === "following")
+              return {
+                uri: "following",
+                type: "timeline",
+                displayName: "Following",
+                description: "Following",
+                avatar: "",
+                pinned,
+              };
+            const dbFeed = dbFeeds.find((df) => df.uri === uri);
+            if (dbFeed) return { ...dbFeed, pinned };
+            const type = uri.includes("generator") ? "feed" : "list";
+            return {
+              uri: uri,
+              pinned,
+              type,
+              displayName: uri,
+              description: "",
+              avatar: "",
+            };
+          })
+          .toSorted((a, b) => (!a.pinned && b.pinned ? 1 : -1));
+        setFeeds(newFeeds);
+      });
+    updateInfos().then(getInfos).catch(console.error);
+  }, [db, prefFeeds]);
+  return feeds;
+};
+
+const pickFeed = ({
+  uri,
+  displayName,
+  name,
+  description = "",
+  avatar,
+}: (GeneratorRecord | ListRecord) & { uri: string }): DBFeed => {
+  const type = uri.includes("generator") ? "feed" : "list";
+  const dn = (displayName ?? name ?? uri) as string;
+  return {
+    uri,
+    type,
+    displayName: dn,
+    description,
+    avatar: getAvatarLink(uri, avatar),
+  };
+};
+
+interface BlobRef {
+  $type: "blob";
+  ref: {
+    $link: string;
+  };
+  mimeType: `image/${string}`;
+  size: number;
+}
+const getAvatarLink = (uri: string, avatar: unknown) => {
+  if (isBlob(avatar)) {
+    const [repo] = parseAtUri(uri);
+    const {
+      ref: { $link: cid },
+      mimeType,
+    } = avatar;
+    const ext = mimeType.split("/")[1];
+    return `https://cdn.bsky.app/img/avatar/plain/${repo}/${cid}@${ext}`;
+  }
+  return "";
+};
+
+const isBlob = (blob: unknown): blob is BlobRef =>
+  isObj(blob) && "$type" in blob && blob.$type === "blob";
--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,18 @@
-import { GetPostThreadResponse, Profile } from "@/types/bsky";
-import { BSKY_GET_POST_THREAD_API, BSKY_GET_PROFILE_API } from "./const";
+import {
+  FeedView,
+  GetPostThreadResponse,
+  GetRecordResponse,
+  ListView,
+  Profile,
+} from "@/types/bsky";
+import {
+  BSKY_GET_FEED_GENERATORS_API,
+  BSKY_GET_LISTS_API,
+  BSKY_GET_POST_THREAD_API,
+  BSKY_GET_PROFILE_API,
+  BSKY_GET_RECORD_API,
+} from "./const";
+import { parseAtUri } from "./uri";
 
 const parse = (res: Response) => res.json();
 
@@ -7,3 +20,13 @@ export const getProfile: (handle: string) => Promise<Profile> = (handle) =>
   fetch(`${BSKY_GET_PROFILE_API}?actor=${handle}`).then(parse);
 export const getPostThread: (uri: string) => Promise<GetPostThreadResponse> = //
   (uri) => fetch(`${BSKY_GET_POST_THREAD_API}?uri=${uri}`).then(parse);
+export const getRecord: (uri: string) => Promise<GetRecordResponse> = (uri) => {
+  const [repo, collection, rkey] = parseAtUri(uri);
+  const url =
+    `${BSKY_GET_RECORD_API}?repo=${repo}&collection=${collection}&rkey=${rkey}`;
+  return fetch(url).then(parse);
+};
+export const getFeedGenerators: () => Promise<FeedView[]> = () =>
+  fetch(BSKY_GET_FEED_GENERATORS_API).then(parse).then(({ feeds }) => feeds);
+export const getLists: () => Promise<ListView[]> = () =>
+  fetch(BSKY_GET_LISTS_API).then(parse).then(({ lists }) => lists);
