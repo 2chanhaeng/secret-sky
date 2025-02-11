e5a2f747695ccd32cab728d0ac0996b7df32736e
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-12 04:16:26 +0900

    fix NewPost position

--- components/NewPost/index.tsx
+++ components/NewPost/index.tsx
@@ -11,7 +11,7 @@ import PostForm from "../PostForm";
 export default function NewPost() {
   return (
     <Dialog>
-      <DialogTrigger className="sticky bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full">
+      <DialogTrigger className="sticky bottom-4 right-4 float-right p-4 bg-blue-500 text-white rounded-full">
         <SquarePen />
       </DialogTrigger>
       <DialogContent className="max-h-[100svh]">
