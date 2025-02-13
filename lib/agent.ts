da8832292c6e87218c63a1a790a435a178e1c7be
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-02-13 23:04:55 +0900

    fix getAgent error

--- lib/agent.ts
+++ lib/agent.ts
@@ -2,15 +2,15 @@
 
 import { Agent } from "@atproto/api";
 import { NodeOAuthClient } from "@atproto/oauth-client-node";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { sessions } from "./cookie";
 
 export const getAgent: //
   (client: NodeOAuthClient, redirectTo?: string) => Promise<Agent> = async ( //
     client,
     redirectTo = "/",
   ) => {
-    const did = (await sessions.get())?.sub;
+    const did = (await cookies()).get("did")?.value;
     if (!did) redirect(`/auth/login?redirectTo=${redirectTo}`);
     const agent = await client
       .restore(did)
